0453fb343538a066cdc09053537e308f03e5d3b2
Jongrjon <jongrjon@gmail.com>
2021-10-20 16:09:25 +0000

    gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x^=ulsh#e7s5$&-(=w4!p&+1n0hkk(!h4^ts8d)0s$0(yoxmu*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
