dd75c068b915fd75339db4df160c70e64435b812
Jón Helgi <jhek@battenburg.localdomain>
2022-10-26 18:25:47 +0000

    cv filled

--- backend/cv/headers.py
+++ backend/cv/headers.py
@@ -4,4 +4,5 @@ headers = {
     'SK' : {'is':"Kunnátta", 'en':"Skills"},
     'SE' : {'is':"Námskeið", 'en':"Seminars"},
     'RE' : {'is':"Meðmælendur", 'en':"Recommendors"},
+    'HO' : {'is':"Áhugamál og Félagsstörf", 'en':"Hobbies and social activities"}
 }
