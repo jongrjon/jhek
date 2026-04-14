2cf8a3d20ae66808eb34bb235203d4847f0e5bd3
=Jón Helgi <=>
2026-04-14 13:34:19 +0000

    Major update

--- backend/cv/headers.py
+++
@@ -1,8 +0,0 @@
-headers = {
-    'ED' : {'is':"Menntun", 'en':"Education"},
-    'JO' : {'is':"Starfsreynsla", 'en':"Work experience"},
-    'SK' : {'is':"Kunnátta", 'en':"Skills"},
-    'SE' : {'is':"Námskeið", 'en':"Seminars"},
-    'RE' : {'is':"Meðmælendur", 'en':"Recommendors"},
-    'HO' : {'is':"Áhugamál og Félagsstörf", 'en':"Hobbies and social activities"}
-}
